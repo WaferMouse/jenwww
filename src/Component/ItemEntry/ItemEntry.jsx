21446d65c5a833229a356cf61046d10606a8ac04
Jocelyne <gothling@jocely.net>
2025-08-18 14:18:50 +0100

    cleaning up linktree cruft

--- src/Component/ItemEntry/ItemEntry.jsx
+++ src/Component/ItemEntry/ItemEntry.jsx
@@ -1,42 +1,31 @@
 export default function ItemEntry(props) {
   return (
-    <div class="relative h-full" data-id="277897720">
-      <div
-        class="-top[92px] invisible relative block"
-        id="277897720"
-      ></div>
-      <div
-        data-testid="StyledContainer"
-        data-linktype="CLASSIC"
-        class="sc-bdfBwQ sc-dmlrTW fcHYTr imRFLm group"
-        type="CLASSIC"
+    <div>
+      <a
+        href={props.url}
+        target="_blank"
+        rel="noreferrer"
+        data-testid="LinkButton"
+        class="buttony"
+        height="auto"
       >
-        <a
-          href={props.url}
-          target="_blank"
-          rel="noopener"
-          data-testid="LinkButton"
-          class="buttony"
-          height="auto"
-        >
-          <div class="h-full w-full">
-            <div
-              data-testid="LinkThumbnail"
-              type="CLASSIC"
-              class="thumby"
-            >
-              <img
-                data-testid="LinkThumbnailImage"
-                src={props.thumbimg}
-                class="otherthumby"
-              />
-            </div>
-            <p class="sc-hKgILt sc-hiSbYr cxwgnw eHEGrJ">
-              {props.content}
-            </p>
+        <div>
+          <div
+            data-testid="LinkThumbnail"
+            type="CLASSIC"
+            class="thumby"
+          >
+            <img
+              data-testid="LinkThumbnailImage"
+              src={props.thumbimg}
+              class="otherthumby"
+            />
           </div>
-        </a>
-      </div>
+          <p>
+            {props.content}
+          </p>
+        </div>
+      </a>
     </div>
   )
 }
