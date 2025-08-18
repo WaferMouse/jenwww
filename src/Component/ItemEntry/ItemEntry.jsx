7786ff7d50388ce49331d003731f2aad5314b01b
Jocelyne <gothling@jocely.net>
2025-08-18 14:47:38 +0100

    more styling

--- src/Component/ItemEntry/ItemEntry.jsx
+++ src/Component/ItemEntry/ItemEntry.jsx
@@ -1,34 +1,20 @@
 export default function ItemEntry(props) {
   return (
-    <div>
-      <div>
-        <div data-testid="LinkThumbnail" type="CLASSIC" class="thumby">
-          <a
-            href={props.url}
-            target="_blank"
-            rel="noreferrer"
-            class="buttony"
-            height="auto"
-          >
-            <img
-              data-testid="LinkThumbnailImage"
-              src={props.thumbimg}
-              class="otherthumby"
-            />
-            
-            <p>{props.content}</p>
-          </a>
-
-          <a
-            href={props.url}
-            target="_blank"
-            rel="noreferrer"
-            class="buttony"
-            height="auto"
-          >
-          </a>
-        </div>
-      </div>
+    <div data-testid="LinkThumbnail" type="CLASSIC" class="thumby">
+      <a
+        href={props.url}
+        target="_blank"
+        rel="noreferrer"
+        class="buttony"
+        height="auto"
+      >
+        <img
+          data-testid="LinkThumbnailImage"
+          src={props.thumbimg}
+          class="otherthumby"
+        />
+        <p>{props.content}</p>
+      </a>
     </div>
   );
 }
